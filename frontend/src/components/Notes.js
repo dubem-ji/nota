020ac0bae319eb6143f3a4bf15652b32cbe209cf
Joy Iroegbu <mapt.dubem@gmail.com>
2023-05-13 20:12:40 +0100

    create notes component

---
+++ frontend/src/components/Notes.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Notes = () => {
+    return (
+        <div className='notes'>
+            <div className="sidebar">
+                
+            </div>
+            <div className="all-notes">
+
+            </div>
+        </div>
+    );
+};
+
+export default Notes;
