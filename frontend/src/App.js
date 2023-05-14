178fcd28849d80da501405dc473fee070bb1da5f
Joy Iroegbu <mapt.dubem@gmail.com>
2023-05-14 19:08:15 +0100

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,53 @@
-import React from "react";
+import React, {useState} from "react";
 import Header from "./components/header";
 import Main from "./components/Main/Main";
+import Confirm from "./components/Confirmation/Confirm";
 
 function App() {
+    const [showNotes, setShowNotes] = useState(true);
+    const [showFolder, setShowFolder] = useState(false);
+    const [showRecent, setShowRecent] = useState(false);
+    const [showNoteInput, setShowNoteInput] = useState(false);
+    const [showConfirm, setShowConfirm] = useState(false);
+
+    // #Toggle App Sections
+    const toggleShowNotes = () => {
+        setShowNotes(true);
+        setShowFolder(false);
+        setShowRecent(false);
+        setShowNoteInput(false);
+    };
+    const toggleShowFolder = () => {
+        setShowFolder(true);
+        setShowNotes(false);
+        setShowRecent(false);
+        setShowNoteInput(false);
+    };
+    const toggleShowRecent = () => {
+        setShowRecent(true);
+        setShowFolder(false);
+        setShowNotes(false);
+        setShowNoteInput(false);
+    };
+    const toggleShowNoteInput = () => {
+        setShowNoteInput(true);
+        setShowRecent(false);
+        setShowFolder(false);
+        setShowNotes(false);
+    };
+    const togglesShowConfirm = () => {
+        setShowConfirm(!showConfirm)
+    }
+    
+
     return (
         <div className="app">
-            <Header />
-            <Main />
+            {showConfirm && (
+                <Confirm togglesShowConfirm={togglesShowConfirm} />
+            )}
+            
+            <Header toggleShowNoteInput={toggleShowNoteInput} />
+            <Main showNotes={showNotes} toggleShowNotes={toggleShowNotes} showFolder={showFolder} toggleShowFolder={toggleShowFolder} showRecent={showRecent} toggleShowRecent={toggleShowRecent} showNoteInput={showNoteInput} toggleShowNoteInput={toggleShowNoteInput} togglesShowConfirm={ togglesShowConfirm} />
         </div>
     );
 };
