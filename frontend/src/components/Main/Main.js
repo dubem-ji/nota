7fbdc187da4023eda949dc2027e7ff72835167c0
Joy Iroegbu <mapt.dubem@gmail.com>
2023-05-14 19:08:45 +0100

    Update Main.js

--- frontend/src/components/Main/Main.js
+++ frontend/src/components/Main/Main.js
@@ -2,15 +2,33 @@ import React from 'react';
 import Sidebar from './Sidebar';
 import Notes from '../Notes/Notes';
 
-const Main = () => {
+const Main = ({showNotes, toggleShowNotes, showFolder, toggleShowFolder, showRecent, toggleShowRecent, showNoteInput, toggleShowNoteInput, togglesShowConfirm}) => {
+    
     return (
         <div className='main'>
             <div className="sidebar">
-                <Sidebar />
+                <Sidebar toggleShowNotes={toggleShowNotes} toggleShowFolder={toggleShowFolder} toggleShowRecent={toggleShowRecent} toggleShowNoteInput={ toggleShowNoteInput} />
             </div>
-            <div className="all-notes">
-                <Notes />
-            </div>
+            {showNotes && (
+                <div className="all-notes">
+                    <Notes toggleShowNoteInput={toggleShowNoteInput} togglesShowConfirm={ togglesShowConfirm} />
+                </div>
+            )}
+             {showFolder && (
+                <div className="all-notes">
+                    <h3>All folders</h3>
+                </div>
+            )}
+             {showRecent && (
+                <div className="all-notes">
+                    <h3>Recent</h3>
+                </div>
+            )}
+            {showNoteInput && (
+                <div className="all-notes">
+                    <h3>Edit note here</h3>
+                </div>
+            )}
         </div>
     );
 };
