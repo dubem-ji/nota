656f57561f1ddeede19898c37650941f55a45a0e
Joy Iroegbu <mapt.dubem@gmail.com>
2023-05-14 13:18:17 +0100

    Create AddLabel.js

---
+++ frontend/src/components/Labels.js/AddLabel.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import AddIcon from '@mui/icons-material/Add';
+
+const AddLabel = () => {
+    return (
+        <div className='create-labels'>
+            <AddIcon sx={{ fontSize: '1rem' }} />
+            <h4>Labels</h4>
+        </div>
+    );
+};
+
+export default AddLabel;
