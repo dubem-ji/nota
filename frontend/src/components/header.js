dfa501991306ce0102cd1257c6fce3742e0cebef
Joy Iroegbu <mapt.dubem@gmail.com>
2023-05-14 19:08:19 +0100

    Update header.js

--- frontend/src/components/header.js
+++ frontend/src/components/header.js
@@ -1,23 +1,29 @@
 import React from 'react';
 import Createnote from './Notes/createnote';
 
-const Header = () => {
+const Header = ({ toggleShowNoteInput }) => {
     return (
         <header className='header'>
             <div className="container">
                 <div className="brand">
                     <h3>nota</h3>
                 </div>
-                <div className="profile">
-                    <Createnote />
-                    <div className="pic">
-
+                <div className="r-header">
+                    <div className="add-note" onClick={
+                        () => toggleShowNoteInput()
+                    }>
+                        <Createnote />
                     </div>
-                    <div className="name">
-                        <h4>Dubem</h4>
+                    <div className="profile">
+                        <div className="pic">
+
+                        </div>
+                        <div className="name">
+                            <h4>Dubem</h4>
+                        </div>
                     </div>
                 </div>
-          </div>
+            </div>
         </header>
     );
 };
