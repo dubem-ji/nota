0ef0c07b56db3145c2a257be92420c9a4ad75658
Joy Iroegbu <mapt.dubem@gmail.com>
2023-05-15 12:46:15 +0100

    Update note.js

--- frontend/src/components/Notes/note.js
+++ frontend/src/components/Notes/note.js
@@ -5,26 +5,29 @@ import FolderIcon from '@mui/icons-material/Folder';
 import BorderColorIcon from '@mui/icons-material/BorderColor';
 import DeleteIcon from '@mui/icons-material/Delete';
 
-const Note = ({ toggleShowNoteInput, togglesShowConfirm}) => {
+const Note = ({ toggleShowNoteInput, togglesShowConfirm, note }) => {
+  const tags  = note.tags
+
   return (
     <div className='note'>
       <div className="note-content">
         <div className="head">
-          <h5>20 APR</h5>
-          <h4>The Title</h4>
+          <h5>{note.date}</h5>
+          <h4>{note.title}</h4>
         </div>
         <div className="tags">
-          <Label />
-          <Label />
+          {tags.map((tag) => [
+            <Label key={tag.indexOf()} tag={ tag} />
+          ])}
         </div>
         <div className="content">
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum sequi enim iusto esse. Animi dignissimos amet corrupti, minus inventore quibusdam!</p>
+          <p>{note.body }</p>
         </div>
       </div>
       <div className="note-icon">
         <div className="note-folder">
           <FolderIcon sx={{fontSize:'1rem'}} />
-          <h5>Personal</h5>
+          <h5>{note.folder}</h5>
         </div>
         <div className="note-btns">
           <div onClick={
