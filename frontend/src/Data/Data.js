03dcccc735370f43a418647adb06a6f16d681071
Joy Iroegbu <mapt.dubem@gmail.com>
2023-05-17 11:55:27 +0100

    Update Data.js

--- frontend/src/Data/Data.js
+++ frontend/src/Data/Data.js
@@ -3,9 +3,8 @@ const notes = [
         id: 1,
         title: 'The Road Not Taken',
         body: {
-            value: <><p><span style={{color: 'rgb(255, 0, 0)'}}>The code snippet can be used to get the first 10 words from a string.</span></p><ul><li>You can set </li><li>'getWordStr'</li></ul><p><strong>You can set start</strong></p></>
+            value: <><p><span>The code snippet can be used to get the first 10 words from a string.</span></p><ul><li>You can set </li><li>'getWordStr'</li></ul><p><strong>You can set start</strong></p></>
         },
-        tags: ['poems', 'inspiration'],
         date: '2014-04-03',
         folder: 'personal'
     },
@@ -13,9 +12,8 @@ const notes = [
         id: 2,
         title: 'Death be not proud',
         body: {
-            value: <><p><span style={{color: 'rgb(255, 255, 0)'}}>The code snippet can be used to get the first 10 words from a string.</span></p><ul><li>You can set </li><li>'getWordStr'</li></ul><p><strong>You can set start</strong></p></>
+            value: <><p><span >The code snippet can be used to get the first 10 words from a string.</span></p><ul><li>You can set </li><li>'getWordStr'</li></ul><p><strong>You can set start</strong></p></>
         },
-        tags: ['poems', 'dark'],
         date: '2015-05-03',
         folder: 'school'
     },
@@ -24,11 +22,40 @@ const notes = [
         title: 'Ozymandias',
         body: {
             value: <><p>The code snippet can be used to get the first 10 words from a string. You can set start and end points in the 'getWordStr' function. We have applied a start point from 0 and an endpoint to 10 The code snippet can be used to get the first 10 words from a string. You can set start and end points in the 'getWordStr' function. We have applied a start point from 0 and an endpoint to 10 The code snippet can be used to get the first 10 words from a string. You can set start and end points in the 'getWordStr' function. We have applied a start point from 0 and an endpoint to 10</p>
-          <p><em>fgdfhghg</em></p></>
+                <p><em>fgdfhghg</em></p></>
         },
-        tags: ['poems', 'dark'],
         date: '2015-08-13',
         folder: 'school'
+    },
+    {
+        id: 4,
+        title: 'Ozymandias',
+        body: {
+            value: <><p>The code snippet can be used to get the first 10 words from a string. You can set start and end points in the 'getWordStr' function. We have applied a start point from 0 and an endpoint to 10 The code snippet can be used to get the first 10 words from a string. You can set start and end points in the 'getWordStr' function. We have applied a start point from 0 and an endpoint to 10 The code snippet can be used to get the first 10 words from a string. You can set start and end points in the 'getWordStr' function. We have applied a start point from 0 and an endpoint to 10</p>
+                <p><em>fgdfhghg</em></p></>
+        },
+        date: '2015-08-13',
+        folder: 'school'
+    },
+    {
+        id: 6,
+        title: 'Ozymandias',
+        body: {
+            value: <><p>The code snippet can be used to get the first 10 words from a string. You can set start and end points in the 'getWordStr' function. We have applied a start point from 0 and an endpoint to 10 The code snippet can be used to get the first 10 words from a string. You can set start and end points in the 'getWordStr' function. We have applied a start point from 0 and an endpoint to 10 The code snippet can be used to get the first 10 words from a string. You can set start and end points in the 'getWordStr' function. We have applied a start point from 0 and an endpoint to 10</p>
+                <p><em>fgdfhghg</em></p></>
+        },
+        date: '2015-08-13',
+        folder: 'work'
+    },
+    {
+        id: 7,
+        title: 'Ozymandias',
+        body: {
+            value: <><p>The code snippet can be used to get the first 10 words from a string. You can set start and end points in the 'getWordStr' function. We have applied a start point from 0 and an endpoint to 10 The code snippet can be used to get the first 10 words from a string. You can set start and end points in the 'getWor</p>
+                <p><em>fgdfhghg</em></p></>
+        },
+        date: '2015-08-13',
+        folder: 'work'
     },
 ];
 
@@ -40,15 +67,19 @@ const user = {
         {
             id: 1,
             name: 'personal',
-            color: 'red',
+            color: 'turquoise',
         },
         {
-            id: 1,
+            id: 2,
             name: 'school',
-            color: 'blue',
-        }
+            color: 'chartreuse',
+        },
+        {
+            id: 3,
+            name: 'work',
+            color: 'crimson',
+        },
     ],
-    tag: ['poems', 'house', 'inspiration']
 }
 
 export { notes, user };
