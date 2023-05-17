c2223d361455fb9cf6fb5451c9533ef640419b0a
Joy Iroegbu <mapt.dubem@gmail.com>
2023-05-17 11:55:17 +0100

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React, {useState} from "react";
-import { notes } from "./Data/Data";
+import { notes, user } from "./Data/Data";
 import Header from "./components/header";
 import Main from "./components/Main/Main";
 import Confirm from "./components/Confirmation/Confirm";
@@ -8,37 +8,33 @@ import Createfolder from "./components/Folders/createfolder";
 function App() {
     const [showNotes, setShowNotes] = useState(true);
     const [showFolder, setShowFolder] = useState(false);
-    const [showRecent, setShowRecent] = useState(false);
     const [showNoteInput, setShowNoteInput] = useState(false);
     const [showConfirm, setShowConfirm] = useState(false);
     const [confirm, setConfirm] = useState(false);
     const [showCreateFolder, setShowCreateFolder] = useState(false);
     const [notesData, setNotesData] = useState(notes);
+    const [userData, setUserData] = useState(user);
+    const [showFolderContent, setShowFolderContent] = useState(false);
+    const [currentFolder, setCurrentFolder] = useState(''); 
 
     // #Toggle App Sections
     const toggleShowNotes = () => {
         setShowNotes(true);
         setShowFolder(false);
-        setShowRecent(false);
         setShowNoteInput(false);
+        setShowFolderContent(false);
     };
     const toggleShowFolder = () => {
         setShowFolder(true);
         setShowNotes(false);
-        setShowRecent(false);
         setShowNoteInput(false);
-    };
-    const toggleShowRecent = () => {
-        setShowRecent(true);
-        setShowFolder(false);
-        setShowNotes(false);
-        setShowNoteInput(false);
+        setShowFolderContent(false);
     };
     const toggleShowNoteInput = () => {
         setShowNoteInput(true);
-        setShowRecent(false);
         setShowFolder(false);
         setShowNotes(false);
+        setShowFolderContent(false);
     };
     const togglesShowConfirm = () => {
         setShowConfirm(!showConfirm)
@@ -46,6 +42,14 @@ function App() {
     const togglesShowCreateFolder = () => {
         setShowCreateFolder(!showCreateFolder)
     };
+    const openFolderContent = (folderName) => {
+        setShowFolderContent(true);
+        setShowNotes(false);
+        setShowFolder(false);
+        setShowNoteInput(false);
+
+        setCurrentFolder(folderName);
+    }
 
     // #confirmOption 
     const confirmOption = (option) => {
@@ -69,6 +73,11 @@ function App() {
         }
     };
 
+    // #Edit User Data
+    const editUserData = () => [
+        setUserData(userData)
+    ]
+
     return (
         <div className="app">
             {showConfirm && (
@@ -78,8 +87,8 @@ function App() {
                 <Createfolder togglesShowCreateFolder={togglesShowCreateFolder} />
             )}
             
-            <Header toggleShowNoteInput={toggleShowNoteInput} />
-            <Main showNotes={showNotes} toggleShowNotes={toggleShowNotes} showFolder={showFolder} toggleShowFolder={toggleShowFolder} showRecent={showRecent} toggleShowRecent={toggleShowRecent} showNoteInput={showNoteInput} toggleShowNoteInput={toggleShowNoteInput} togglesShowConfirm={togglesShowConfirm} notesData={notesData} togglesShowCreateFolder={togglesShowCreateFolder} deleteNote={deleteNote} addNote={ addNote} />
+            <Header toggleShowNoteInput={toggleShowNoteInput} userData={ userData} />
+            <Main showNotes={showNotes} toggleShowNotes={toggleShowNotes} showFolder={showFolder} toggleShowFolder={toggleShowFolder} showNoteInput={showNoteInput} toggleShowNoteInput={toggleShowNoteInput} togglesShowConfirm={togglesShowConfirm} notesData={notesData} togglesShowCreateFolder={togglesShowCreateFolder} deleteNote={deleteNote} addNote={addNote} userData={userData} openFolderContent={openFolderContent} showFolderContent={showFolderContent} currentFolder={currentFolder} editUserData={ editUserData} />
         </div>
     );
 };
