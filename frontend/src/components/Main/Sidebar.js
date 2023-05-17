baad8aca489f814ef15f70ca4b7dad890b40c0a7
Joy Iroegbu <mapt.dubem@gmail.com>
2023-05-17 11:56:34 +0100

    Update Sidebar.js

--- frontend/src/components/Main/Sidebar.js
+++ frontend/src/components/Main/Sidebar.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import Createnote from '../Notes/createnote';
-import AddLabel from '../Labels.js/AddLabel';
 
 import NotesIcon from '@mui/icons-material/Notes';
 import FolderCopySharpIcon from '@mui/icons-material/FolderCopySharp';
-import StarIcon from '@mui/icons-material/Star';
 
 const Sidebar = ({toggleShowNotes, toggleShowFolder, toggleShowRecent, toggleShowNoteInput}) => {
     return (
@@ -22,17 +20,8 @@ const Sidebar = ({toggleShowNotes, toggleShowFolder, toggleShowRecent, toggleSho
                     <FolderCopySharpIcon sx={{ fontSize: '1rem', color: '#42ba96' }} />
                     <h4>Folders</h4>
                 </div>
-                <div className="menu-item recents-icon" onClick={
-                    () => toggleShowRecent()
-                }>
-                    <StarIcon sx={{ fontSize: '1rem', color: '#f3e36f' }} />
-                    <h4 >Recent</h4>
-                </div>
             </div>
             <div className="bottom">
-                <div className="menu-item add-label">
-                    <AddLabel />
-                </div>
                 <div className="menu-item add-note" onClick={
                     () => toggleShowNoteInput()
                 }>
