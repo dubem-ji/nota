7c182f691c95c51eab8c33ff349bb270e9f56d5c
Joy Iroegbu <mapt.dubem@gmail.com>
2023-06-16 12:52:29 +0100

    Create NotesContext.js

---
+++ frontend/src/Context/NotesContext.js
@@ -0,0 +1,17 @@
+import { createContext, useState } from 'react';
+import { notes } from "../Data/Data";
+
+const NoteContext = createContext();
+
+export const NoteProvider = ({ children }) => {
+    const { notesData, setNotesData } = useState(notes);
+
+    
+    return <NoteContext.Provider value={{
+        notesData,
+    }}>
+        {children}
+    </NoteContext.Provider>
+};
+
+export default NoteContext;
