05fbf13f93469a9177456eee686d06ce162b8d3e
Joy Iroegbu <mapt.dubem@gmail.com>
2023-06-16 13:58:36 +0100

    Create NotesMain.js

---
+++ frontend/src/Components/Notes/NotesMain.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import AllNotes from './AllNotes';
+
+const NotesMain = () => {
+    return (
+        <div className="notes-main">
+          <AllNotes />
+        </div>
+    )
+};
+
+export default NotesMain;
