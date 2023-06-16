1d3e38393c40c586bd402840888b7856837aab81
Joy Iroegbu <mapt.dubem@gmail.com>
2023-06-16 13:58:38 +0100

    Create AllNotes.js

---
+++ frontend/src/Components/Notes/AllNotes.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import NavBar from '../General/NavBar';
+
+const AllNotes = () => {
+  return (
+    <div className='all-notes'>
+          <NavBar />
+          <div className="body">
+              
+          </div>
+    </div>
+  )
+}
+
+export default AllNotes;
