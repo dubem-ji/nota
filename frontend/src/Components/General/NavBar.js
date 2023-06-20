dbdce4ad31192d78c3a8fdaa9bd4144f883bf13c
Joy Iroegbu <mapt.dubem@gmail.com>
2023-06-20 10:06:26 +0100

    Update NavBar.js

--- frontend/src/Components/General/NavBar.js
+++ frontend/src/Components/General/NavBar.js
@@ -4,16 +4,16 @@ import {user} from '../../Data/Data'
 const NavBar = () => {
     return (
         <div className="nav-bar">
+            <div className="brand">
+                  <h3>nota</h3>
+            </div>
             <div className="user">
+                <div className="username">
+                    <h4>{ user.username}</h4>
+                </div>
                 <div className="avatar">
                       
                 </div>
-                <div className="username">
-                    <h5>{ user.username}</h5>
-                </div>
-            </div>
-            <div className="brand">
-                  <h3>nota</h3>
             </div>
         </div>
     )
