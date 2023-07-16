a89f75deeb91e0fbb99f79d7a02c26fb9b71dc26
Joy Iroegbu <mapt.dubem@gmail.com>
2023-07-16 14:23:29 +0100

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { NoteProvider } from "./context/NotesContext";
+import { NoteProvider } from "./context/NotaContext";
 
 
 function App() {
@@ -8,15 +8,13 @@ function App() {
 
     return (
         <Router>
-            <div className="app">
-                <Routes>
-                    <Route exact path="/" element={
-                        <>
-                            <h4>Home</h4>
-                        </>
-                    } />
-                </Routes>
-            </div>
+            <Routes>
+                <Route exact path="/" element={
+                    <>
+                        <h4>Home</h4>
+                    </>
+                } />
+            </Routes>
         </Router>
     );
 };
